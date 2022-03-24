bc82d388d82e92360e1783e05f926e73f324edca
Andrew Cox <andmcox19@gmail.com>
2022-03-24 09:28:45 -0500

    testing app to show chromosome image

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,24 +9,24 @@ import matplotlib.pyplot as plt
 import warnings 
 import logging
 
-data_path = 'streamlit_app_images/'=
-model_path = 'resources/models/'
+# data_path = 'streamlit_app_images/'=
+# model_path = 'resources/models/'
 img = data_path + '04_inv16.tiff'
-
-data = ImageDataLoaders.from_folder(data_path, 
-                                    train='train',
-                                    valid='test, 
-                                    batch_tfms=[*aug_transforms(flip_vert=False, max_lighting=0.1, max_zoom=1.0, max_warp=0, p_affine=0),Normalize.from_stats(*imagenet_stats)],
-                                    bs=32, 
-                                    resize_method=ResizeMethod.Squish, 
-                                    size=(512, 512), 
-                                    num_workers=16
-                                    )
-
-learn = cnn_learner(data, models.resnet50, metrics=[error_rate])
-learn.load(model_path + '020232021_512_res50_round1.h5.pth')
-learn.predict(img)
-
-gcam = GradCam(learn, img, None)
-gcam.plot(full_size=True, plot_original=False, figsize(12, 6))
 st.img(img)
+
+# data = ImageDataLoaders.from_folder(data_path, 
+#                                     train='train',
+#                                     valid='test, 
+#                                     batch_tfms=[*aug_transforms(flip_vert=False, max_lighting=0.1, max_zoom=1.0, max_warp=0, p_affine=0),Normalize.from_stats(*imagenet_stats)],
+#                                     bs=32, 
+#                                     resize_method=ResizeMethod.Squish, 
+#                                     size=(512, 512), 
+#                                     num_workers=16
+#                                     )
+
+# learn = cnn_learner(data, models.resnet50, metrics=[error_rate])
+# learn.load(model_path + '020232021_512_res50_round1.h5.pth')
+# learn.predict(img)
+
+# gcam = GradCam(learn, img, None)
+# gcam.plot(full_size=True, plot_original=False, figsize(12, 6))
