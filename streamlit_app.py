a5eaacce15550129bdccd296cd5fb70878334ef4
Andrew Cox <andmcox19@gmail.com>
2022-03-24 10:11:44 -0500

    Trying to load model with PyTorch.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
-from fastai.vision.all import * 
-from fastai.vision.data import * 
-from fastai.vision.core import * 
-from fastai2_extensions.interpret.all import *
+# from fastai.vision.all import * 
+# from fastai.vision.data import * 
+# from fastai.vision.core import * 
+# from fastai2_extensions.interpret.all import *
+
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings 
@@ -13,17 +15,18 @@ data_path = 'streamlit_app_images/'
 model_path = 'resources/models/'
 img = data_path + '04_inv16.tiff'
 st.image(img)
-data_path = Path('streamlit_app_images/')
-data = ImageDataLoaders.from_folder(data_path, train_name='train',
-                                    valid_name='test', 
-                                    batch_tfms=[*aug_transforms(flip_vert=False, max_lighting=0.1, max_zoom=1.0, max_warp=0, p_affine=0),Normalize.from_stats(*imagenet_stats)], 
-                                    bs=32, 
-                                    resize_method=ResizeMethod.Squish, 
-                                    size=(512, 512),
-                                    num_workers=2)
+# data_path = Path('streamlit_app_images/')
+# data = ImageDataLoaders.from_folder(data_path, train_name='train',
+#                                     valid_name='test', 
+#                                     batch_tfms=[*aug_transforms(flip_vert=False, max_lighting=0.1, max_zoom=1.0, max_warp=0, p_affine=0),Normalize.from_stats(*imagenet_stats)], 
+#                                     bs=32, 
+#                                     resize_method=ResizeMethod.Squish, 
+#                                     size=(512, 512),
+#                                     num_workers=2)
 
-learn = cnn_learner(data, models.resnet50, metrics=[error_rate])
-# learn.load(model_path + '020232021_512_res50_round1.h5.pth')
+# learn = cnn_learner(data, models.resnet50, metrics=[error_rate])
+
+torch.load(model_path + '020232021_512_res50_round1.h5.pth')
 # learn.predict(img)
 
 # gcam = GradCam(learn, img, None)
